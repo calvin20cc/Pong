bfa2a1fb30b4dac1e068c8319759bef59f1a8a9f
calvin20cc <calvin20cc@hotmail.com>
2018-05-20 09:48:17 -0400

    first commit

---
+++ PongAnimation.java
@@ -0,0 +1,113 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class PongAnimation implements ActionListener, MouseListener, MouseMotionListener, KeyListener{
+  // Properties
+  public JFrame theframe;
+  public JPong thepanel; //Instead of JPanel
+  public Timer thetimer;
+  
+  // Methods
+  public void actionPerformed(ActionEvent evt){
+    if(thepanel.blnrepaint == true){
+      if(evt.getSource() == thetimer){
+        thepanel.repaint();
+      }
+    }
+  }
+  
+  public void mousePressed(MouseEvent evt){
+    //System.out.println("pressed");
+  }
+  
+  public void mouseReleased(MouseEvent evt){
+    //System.out.println("released");
+  }
+  
+  public void mouseClicked(MouseEvent evt){
+//    System.out.println("clicked");
+//    System.out.println(evt.getX());
+//    System.out.println(evt.getY());
+//    System.out.println(evt.getButton());
+    if(evt.getX() > 0 && evt.getX() < 100 && evt.getY() > 0 && evt.getY() < 100 && evt.getButton() == 3){
+      
+    }
+  }
+  
+  public void mouseExited(MouseEvent evt){
+    //System.out.println("exited");
+  }
+  
+  public void mouseEntered(MouseEvent evt){
+    //System.out.println("entered");
+  }
+  
+  public void mouseDragged(MouseEvent evt){
+    //System.out.println("mouse dragged (" + evt.getX() + "," + evt.getY() + ")");
+  }
+  
+  public void mouseMoved(MouseEvent evt){
+    //System.out.println("mouse moved (" + evt.getX() + "," + evt.getY() + ")");
+
+  }
+  
+  public void keyPressed(KeyEvent evt){
+    System.out.println(evt.getKeyCode() + " key pressed");
+    if(evt.getKeyCode() == KeyEvent.VK_UP){
+      thepanel.blnuppressed = true;
+    }else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
+      thepanel.blndownpressed = true;
+    }
+    if(evt.getKeyCode() == KeyEvent.VK_W){
+      thepanel.blnwpressed = true;
+    }else if(evt.getKeyCode() == KeyEvent.VK_S){
+      thepanel.blnspressed = true;
+    }
+    if(evt.getKeyCode() == KeyEvent.VK_SPACE){
+      thepanel.blnStart = true;
+    }
+  }
+  
+  public void keyReleased(KeyEvent evt){
+    //System.out.println(evt.getKeyCode() + " key released");
+    if(evt.getKeyCode() == KeyEvent.VK_UP){
+      thepanel.blnuppressed = false;
+    }else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
+      thepanel.blndownpressed = false;
+    }
+    if(evt.getKeyCode() == KeyEvent.VK_W){
+      thepanel.blnwpressed = false;
+    }else if(evt.getKeyCode() == KeyEvent.VK_S){
+      thepanel.blnspressed = false;
+    }
+  }
+  
+  public void keyTyped(KeyEvent evt){
+    //System.out.println(evt.getKeyChar() + " key typed");
+  }
+  // Constructors
+  public PongAnimation(){
+    theframe = new JFrame("Let's Play Some Pong!!!");
+    theframe.setSize(800, 600);
+    theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    thepanel = new JPong();
+    thepanel.addMouseListener(this);
+    thepanel.addMouseMotionListener(this);
+    theframe.addKeyListener(this);
+    thepanel.setLayout(null);
+    
+    theframe.setContentPane(thepanel);
+    theframe.setVisible(true);
+    
+    thetimer = new Timer(1000/30, this);
+    thetimer.start();
+  }
+  
+  // Main method
+  public static void main(String[] args){
+    PongAnimation ap = new PongAnimation();
+  }
+}
